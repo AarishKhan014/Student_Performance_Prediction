b0f5c405f19c8467f90fd42481d676ecbd4c28b1
Aarish Khan <aarishk140400@gmail.com>
2025-12-10 13:12:47 +0530

    Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import pandas as pd
+from src.logger import logging
+from src.exception import CustomException
+from dataclasses import dataclass
+
+from sklearn.model_selection import train_test_split
+
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path = os.path.join('artifacts', 'train.csv')
+    test_data_path = os.path.join('artifacts', 'test.csv')
+    raw_data_path = os.path.join('artifacts', 'raw.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+
+    def initiate_data_ingestion(self):
+        logging.info("Starting Data Ingestion Process")
+
+        try:
+            #Read Dataset
+            csv_path = os.path.join('notebooks', 'data', 'Student_Performance.csv')
+            
+            if not os.path.exists (csv_path):
+                logging.info('Performance File Not Found')
+                raise CustomException(f'File Not Found {csv_path}', sys)
+            
+            df = pd.read_csv(csv_path)
+            logging.info (f'Dataset Read Successfully With Shape {df.shape}')
+
+            ## Create Artifacts Folder If It Does'nt Exist
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
+
+            ## Save Raw File
+            df.to_csv(self.ingestion_config.raw_data_path, index=False)
+            logging.info(f'Raw Data Saved At {self.ingestion_config.raw_data_path}')
+
+            ##Split Train And Test
+            train_set, test_set = train_test_split(df, test_size=0.25, random_state=786)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False)
+            logging.info(f'Train Data Saved At {self.ingestion_config.train_data_path}')
+            logging.info(f'Test Data Saved At {self.ingestion_config.test_data_path}')
+
+            logging.info('Data Ingestion Completed Successfully')
+            return self.ingestion_config.train_data_path, self.ingestion_config.test_data_path
+
+
+        except Exception as e:
+            logging.info ("Exception Occured In Data Ingestion")
+            raise CustomException(e, sys)
+        
+## For Testing
+# if __name__ == '__main__':
+#     obj = DataIngestion()
+#     train_data_path, test_data_path = obj.initiate_data_ingestion()
--- src/logger.py
+++ src/logger.py
@@ -4,12 +4,12 @@ from datetime import datetime
 
 
 ## Create Logs Directory
-logs_dir = os.join.path(os.getcwd(), 'logs')
+logs_dir = os.path.join(os.getcwd(), 'logs')
 os.makedirs(logs_dir, exist_ok=True)
 
 ## Create TimeStamp File
-LOG_FILE = f"{datetime.now().strftime('%d_%m_%Y_%H:%M:%S')}.log"
-LOG_FILE_PATH = os.join.path(logs_dir, LOG_FILE)
+LOG_FILE = f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log"
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
 
 ## Configure Logging
 logging.basicConfig(
